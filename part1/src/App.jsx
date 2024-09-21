4654312c0b4453bc25aef5a1273310e4ce4337dd
Joshua Olubori <69902377+JoshuaOlubori@users.noreply.github.com>
2024-09-21 17:34:28 +0000

    test

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,16 +1,71 @@
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  // COMPONENTS
+
+  const Header = (props) => {
+    return <h1>{props.courseTitle}</h1>;
+  };
+  //
+  const Part = (props) => {
+    return (
+      <div>
+        <span>{props.part.name}</span> <span>{props.part.exercises}</span>
+      </div>
+    );
+  };
+  //
+  const Content = (props) => {
+    return (
+      <>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
+      </>
+    );
+  };
+  //
+  const Total = (props) => {
+    return (
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+    );
+  };
+
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <Header courseTitle={course.name} />
+      <Content
+        parts={course.parts}
+        // // exercises1={exercises1}
+        //  part2={part2}
+        // // exercises2={exercises2}
+        //  part3={part3}
+        // // exercises3={exercises3}
+      />
+      <Total
+       parts={course.parts}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
