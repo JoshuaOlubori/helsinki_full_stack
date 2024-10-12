bc89b29e72dbc3822ffb2a7fca90ddad5d466f21
Joshua Olubori <69902377+JoshuaOlubori@users.noreply.github.com>
2024-10-12 17:21:36 +0000

    course info part 7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,51 +1,51 @@
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   // COMPONENTS
 
-  const Header = (props) => {
-    return <h1>{props.courseTitle}</h1>;
+  const Header = ({ courseTitle }) => {
+    return <h1>{courseTitle}</h1>;
   };
   //
-  const Part = (props) => {
+  const Part = ({ partName, partExercises }) => {
     return (
       <div>
-        <span>{props.part.name}</span> <span>{props.part.exercises}</span>
+        <span>{partName}</span> <span>{partExercises}</span>
       </div>
     );
   };
   //
-  const Content = (props) => {
+  const Content = ({ parts }) => {
     return (
       <>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]} />
-        <Part part={props.parts[2]} />
+        {parts.map((part) => (
+          <Part partName={part.name} partExercises={part.exercises} />
+        ))}
       </>
     );
   };
   //
-  const Total = (props) => {
+  const Total = ({ parts }) => {
     return (
       <p>
-        Number of exercises{" "}
-        {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        Total of {parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+        exercises
       </p>
     );
   };
@@ -53,17 +53,8 @@ const App = () => {
   return (
     <div>
       <Header courseTitle={course.name} />
-      <Content
-        parts={course.parts}
-        // // exercises1={exercises1}
-        //  part2={part2}
-        // // exercises2={exercises2}
-        //  part3={part3}
-        // // exercises3={exercises3}
-      />
-      <Total
-       parts={course.parts}
-      />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
