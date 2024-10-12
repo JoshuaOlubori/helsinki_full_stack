9020c84725f3f546a4dc5bc28e5c593034e8178a
Joshua Olubori <69902377+JoshuaOlubori@users.noreply.github.com>
2024-10-12 20:36:28 +0000

    phonebook step 2

--- part1/phonebook/src/App.jsx
+++ part1/phonebook/src/App.jsx
@@ -8,11 +8,68 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  function areTheseObjectsEqual(first, second) {
+    "use strict";
+    // from https://www.joshbritz.co/blog/why-its-so-hard-to-check-object-equality
+
+    if (
+      first === null ||
+      first === undefined ||
+      second === null ||
+      second === undefined
+    ) {
+      return first === second;
+    }
+
+    if (first.constructor !== second.constructor) {
+      return false;
+    }
+
+    if (first instanceof Function || first instanceof RegExp) {
+      return first === second;
+    }
+
+    if (first === second || first.valueOf() === second.valueOf()) {
+      return true;
+    }
+
+    if (first instanceof Date) return false;
+
+    if (Array.isArray(first) && first.length !== second.length) {
+      return false;
+    }
+
+    if (!(first instanceof Object) || !(second instanceof Object)) {
+      return false;
+    }
+
+    const firstKeys = Object.keys(first);
+
+    const allKeysExist = Object.keys(second).every(
+      (i) => firstKeys.indexOf(i) !== -1
+    );
+
+    const allKeyValuesMatch = firstKeys.every((i) =>
+      areTheseObjectsEqual(first[i], second[i])
+    );
+
+    return allKeysExist && allKeyValuesMatch;
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const newObj = { name: newName };
-    setPersons(persons.concat(newObj));
-    setNewName("");
+    const matchingObjs = persons.filter((person) =>
+      areTheseObjectsEqual(newObj, person)
+    );
+    console.log(matchingObjs);
+
+    if (matchingObjs.length != 0) {
+      alert(`${newObj.name} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(newObj));
+      setNewName("");
+    }
   };
   return (
     <div>
